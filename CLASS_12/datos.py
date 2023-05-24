9723c1cd2ada106198fc12a6b637d7e0cbdc4364
qemaalo <134541871+qemaalo@users.noreply.github.com>
2023-05-24 14:34:27 -0400

    Código sin validar.

---
+++ CLASS_12/datos.py
@@ -0,0 +1,13 @@
+# EJEMPLO DE USO DE REPOSITORIO
+
+
+print("Datos Personales")
+print("------------------ \n")
+
+vnom = input("Ingresa tu nombre: ")
+vedad = input("Ingresa tu edad: ")
+
+print(f"Su nombre es {vnom}")
+print(f"Su edad es {vedad}")
+
+print("Programa finalizado.")
